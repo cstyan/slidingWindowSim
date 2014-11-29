78bc131758da45bd5550369f1a24acc5c4cd90c8
cstyan <callumstyan@gmail.com>
2014-11-29 13:31:50 -0800

    test

--- temp.rb
+++ temp.rb
@@ -1,32 +1,69 @@
 load 'packet.rb'
 
 #get a port isntead of defining it
-port = 2000
+puts "Enter the port #:"
+port = gets.chomp
 
 client = UDPSocket.new
 puts "Enter the network IP:"
 networkIP = gets.chomp
 client.connect(networkIP, port)
 
-puts "enter program state"
-state = gets.chomp
+puts "Enter the window size:"
+winSize = gets.chomp
 
-run = 1
-if(state.to_i == 1)
-    puts "Input an IP"
-    ip = gets.chomp
-    while(run == 1)
-        puts "enter a message"
-        msg = gets.chomp
-        puts "message entered"
-        packet = makePacket(ip, 1, 1, 1, 1, msg)
-        #pass the port here
-        sendPacket(client, networkIP, port, packet)
+#global queue for the window
+$window = Queue.new
+
+#generate the initial window
+def genWindow(initNum, windowSize, destIP)
+    i = 1
+    seqNum = initNum
+    while(i <= windowSize)
+        packet = makePacket(destIP, 1, seqNum, 0)
+        $window.push(packet)
+        seqNum++
+        i++
     end
-else
-    while(run == 1)
-        packet = getPacket(client)
-        puts packet.data
+    return seqNum
+end
+
+def transmit(numPackets, windowSize, destIP)
+    #used to generate new packets to put into window
+    currentSequenceNum = 0
+    genWindow(currentSequenceNum, windowSize, destIP)
+end
+
+def receive
+
+end
+
+
+#main script
+run = 1
+while(run == 1)
+    continue = 1
+
+    puts "Enter program state, 1 for SEND or 2 for RECEIVE:"
+    state = gets.chomp
+    
+    if(state.to_i == 1)
+        puts "Input the IP you would like to send to:"
+        ip = gets.chomp
+        while(continue == 1)
+            puts "Enter the # of packets you would like to send:"
+            num = gets.chomp
+            packet = makePacket(ip, 1, 1, 1, 1)
+            #pass the port here
+            sendPacket(client, networkIP, port, packet)
+        end
+    elsif(state.to_i == 2)
+        while(continue == 1)
+            packet = getPacket(client)
+            puts packet.data
+        end
+    else
+
     end
 end
 
