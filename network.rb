a960db2828c195ee24bb0f1b617043e1eaf2908e
cstyan <callumstyan@gmail.com>
2014-11-29 22:47:53 -0800

    trying pkt drop

--- network.rb
+++ network.rb
@@ -10,6 +10,8 @@ $portOut
 # $portOut = gets.chomp.to_i
 puts "Port:"
 $port = gets.chomp.to_i
+puts "Enter a percentage of packets to be dropped"
+$pktpct = gets.chomp.to_i
 
 network_1 = UDPSocket.new
 #this binds to INADDR_ANY (any incomming IP address)
@@ -19,8 +21,13 @@ run = 1
 
 while(run == 1)
 	#I am an idiot, don't make new packet
+	randomNum = rand(100)
 	packet = getPacket(network_1)
 	puts packet.data
-	sendPacket(network_1, $port, packet)
+	if(randomNum > pktpct)
+		sendPacket(network_1, $port, packet)
+	else
+		puts "Packet dropped"
+	end
 	# sendPacket(network_1, $port, makePacket(packet.destIP, 2, 0, 1))
 end
