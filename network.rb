6473d5c110db6443494b21b00f09da4c06cfd08d
cstyan <callumstyan@gmail.com>
2014-11-29 19:49:41 -0800

    network port

--- network.rb
+++ network.rb
@@ -4,14 +4,16 @@ load 'packet.rb'
 $portIn
 $portOut
 
-puts "Incomming port:"
-$portIn = gets.chomp.to_i
-puts "Outgoing port:"
-$portOut = gets.chomp.to_i
+# puts "Incomming port:"
+# $portIn = gets.chomp.to_i
+# puts "Outgoing port:"
+# $portOut = gets.chomp.to_i
+puts "Port:"
+$port = gets.chomp.to_i
 
 network_1 = UDPSocket.new
 #this binds to INADDR_ANY (any incomming IP address)
-network_1.bind('', $portIn)
+network_1.bind('', $port)
 
 run = 1
 
@@ -19,5 +21,5 @@ while(run == 1)
 	#I am an idiot, don't make new packet
 	packet = getPacket(network_1)
 	puts packet.data
-	sendPacket(network_1, $portOut, makePacket(packet.destIP, 2, 0, 1))
+	sendPacket(network_1, $port, makePacket(packet.destIP, 2, 0, 1))
 end
